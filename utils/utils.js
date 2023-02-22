81041576c9cfc5c6fbad1a3fce82f6878b803627
Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>
2023-02-21 17:39:04 -0700

    Adding retry policy

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const AWS = require("aws-sdk");
 const path = require("path");
+const polly = require("polly-js");
+const { basename } = require('path');
 
 AWS.config.update({ region: process.env.AWS_DEFAULT_REGION });
 var sqs = new AWS.SQS({ apiVersion: process.env.SQS_API_VERSION });
@@ -74,7 +76,7 @@ let deleteMessageFromSQS = async(messageHandle) => {
     await sqs.deleteMessage(params).promise();
 }
 
-exports.receiveAndDeleteFromSQS = async(fileName) => {
+let receiveAndDeleteFromSQS = async(fileName) => {
     console.log(fileName);
     let response = await receiveResponseFromSQS();
     let message = null;
@@ -83,7 +85,7 @@ exports.receiveAndDeleteFromSQS = async(fileName) => {
         var messages = response.body.Messages;
 
         messages.forEach(async element => {
-            console.log(element);
+            // console.log(element);
             if(element.Body.includes(fileName)) {
                 message = element;
                 await deleteMessageFromSQS(element.ReceiptHandle);
@@ -92,17 +94,50 @@ exports.receiveAndDeleteFromSQS = async(fileName) => {
         });
     } else {
         console.log(`Found 0 messages`);
-        let response = {
+        response = {
             statusCode: 204,
         };
     
         return response;
     }
 
-   response = {
-        statusCode: 200,
-        message: message,
-    };
+    if(message === null) {
+        response = {
+            statusCode: 204,
+        };
+    } else {
+        response = {
+            statusCode: 200,
+            message: message
+        };
+    }
 
     return response;
+}
+
+exports.retryResponseSQS = async(fileName) => {
+    try {
+        const loadResponseFromSQS = () => { return polly()
+            .waitAndRetry(2)
+            .executeForPromise(async() => {
+                let receive_response = await receiveAndDeleteFromSQS(
+                    basename(fileName));
+                        // process messages
+                console.log(receive_response);
+                if(receive_response.statusCode == 200) {
+                    return receive_response;
+                }
+                console.log("Retrying... Could not fetch data")
+                return Promise.reject(receive_response);
+            })
+        };
+
+        return await loadResponseFromSQS();
+    }
+    catch(err) {
+        return {
+            statusCode: 204,
+            message: err
+        };
+    }
 }
